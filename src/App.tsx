35f728972adabc96afe506f736fbaa19d37356b2
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-10-27 19:38:15 -0300

    Requisito 3, ok!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
 import Form from './components/Form';
 import './App.css';
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleClick = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div>
       <h1>Gerenciador de senhas</h1>
-      <Form />
+      {showForm ? (
+        <Form onCancelForm={ handleClick } />
+      ) : (
+        <button onClick={ handleClick }>Cadastrar nova senha</button>
+      )}
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,44 +1,37 @@
-function Form() {
+import React from 'react';
+// Props é sempre um objeto
+type FormProps = {
+  onCancelForm: () => void,
+};
+
+function Form({ onCancelForm } : FormProps) {
   return (
-    <form>
+    <div>
+      <form>
+        <label>
+          Nome do serviço
+          <input type="text" />
+        </label>
 
-      <label>
-        Nome do serviço
-        <input type="text" />
-      </label>
+        <label>
+          Login
+          <input type="text" />
+        </label>
 
-      <label>
-        Login
-        <input type="text" />
-      </label>
+        <label>
+          Senha
+          <input type="password" />
+        </label>
 
-      <label>
-        Senha
-        <input type="password" />
-      </label>
+        <label>
+          URL
+          <input type="text" />
+        </label>
 
-      <label>
-        URL
-        <input type="text" />
-      </label>
-
-      <button>
-        Cadastrar
-      </button>
-
-      <button>
-        Cancelar
-      </button>
-
-      <button>
-        Cancelar
-      </button>
-
-      <button disabled = >
-        Cadastrar nova senha
-      </button>
-
-    </form>
+        <button type="submit">Cadastrar</button>
+        <button onClick={ onCancelForm }>Cancelar</button>
+      </form>
+    </div>
   );
 }
 

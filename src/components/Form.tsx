9ec8d8368ed206213de7af40a660840b77c1e661
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-11-04 21:58:33 -0300

    ok

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -25,7 +25,7 @@ function Form({
 
   const inputValidation = () => {
     const inputServiceName = /^[a-zA-Z0-9 _-]+$/;
-    const inputLogin = /^(?:[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63})$|^[a-zA-Z0-9._-]{2,16}$/;
+    const inputLogin = /.*/;
     const inputPassword = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#$!%^&*?])[a-zA-Z\d@#$!%^&*?]{8,16}$/i;
     const inputURL = /^(ftp|http|https):\/\/[^ "]+$/;
 
